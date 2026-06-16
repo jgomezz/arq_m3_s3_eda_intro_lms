82b98be634d393a61ddc68fa1e0d3f9914e1851a
jgomezz <jgomezz@gmail.com>
2026-06-15 20:49:48 -0500

    update CQRS

--- src/test/java/pe/edu/tecsup/lms/enrollments/application/projection/EnrollmentProjectionTest.java
+++ src/test/java/pe/edu/tecsup/lms/enrollments/application/projection/EnrollmentProjectionTest.java
@@ -1,13 +1,46 @@
 package pe.edu.tecsup.lms.enrollments.application.projection;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pe.edu.tecsup.lms.enrollments.application.query.EnrollmentQueryRepository;
+import pe.edu.tecsup.lms.enrollments.domain.event.StudentEnrolledEvent;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnrollmentProjectionTest {
 
+    private EnrollmentProjection projection;
+    private EnrollmentQueryRepository repository;
+
+    @BeforeEach
+    void init(){
+        this.repository = new EnrollmentQueryRepository();
+        this.projection = new EnrollmentProjection(this.repository);
+    }
+
     @Test
     void onStudentEnrolled() {
+
+        StudentEnrolledEvent event
+                = StudentEnrolledEvent.builder()
+                .enrollmentId("enroll-123")
+                .studentId("student-123")
+                .studentName("student-name")
+                .courseId("course-123")
+                .build();
+
+        this.projection.onStudentEnrolled(event);
+
+        var readModelOpt = this.repository.findByEnrollmentId("enroll-123");
+
+        assertTrue(readModelOpt.isPresent());
+
+        var readModel = readModelOpt.get();
+
+        assertEquals("enroll-123", readModel.getEnrollmentId());
+        assertEquals("student-123", readModel.getStudentId());
+        assertEquals("student-name", readModel.getStudentName());
+
     }
 
     @Test
