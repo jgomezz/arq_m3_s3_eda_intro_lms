7036fbf994f790173cd5927c81fbea6b734745fa
jgomezz <jgomezz@gmail.com>
2026-06-22 23:07:21 -0500

    update Kafka

--- src/main/java/pe/edu/tecsup/lms/courses/domain/event/CourseCreatedEvent.java
+++ src/main/java/pe/edu/tecsup/lms/courses/domain/event/CourseCreatedEvent.java
@@ -2,17 +2,32 @@ package pe.edu.tecsup.lms.courses.domain.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import pe.edu.tecsup.lms.shared.domain.event.DomainEvent;
 
-@AllArgsConstructor
+//@AllArgsConstructor
 @Getter
 @ToString
+//@NoArgsConstructor(force = true)
 public class CourseCreatedEvent extends DomainEvent {
 
-    private final String courseId;
+    private final  String courseId;
     private final String title;
-    private final String instructor;
+    private final  String instructor;
+
+    public CourseCreatedEvent(final String courseId, final String title, final String instructor) {
+        this.courseId = courseId;
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public CourseCreatedEvent() {
+        this.courseId = null;
+        this.title = null;
+        this.instructor = null;
+    }
+
 
     @Override
     public String getKey() {       // SOBREESCRIBIR EL METODO
